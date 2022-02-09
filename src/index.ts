1e7ab76d6b0627a09e5cca237b50002af4e83af6
JhonatanNeves <nevesjn2@gmail.com>
2022-02-09 20:59:34 -0300

    Adicionando typescript e incrimentando a forma de desenvolver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World MF")
+
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res:Response, next:NextFunction) => {
+    res.status(200).send({ foo: 'Hello World'});
+});
+
+app.listen(3000, () => {
+    console.log('Aplicação executando na porta 3000!');
+});
